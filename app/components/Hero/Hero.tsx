9c9f60c3f1f720e6a344e11923e829ceead2cf1b
job monari <jobmonari7@gmail.com>
2023-08-15 01:34:59 +0300

    updating my resume

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -38,12 +38,12 @@ export const Hero = () => {
             </div>
             <div className="flex justify-center">
               <a
-                className="group relative inline-block text-sm font-medium text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                className="group relative inline-block cursor-pointer text-sm font-medium text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
                 onClick={saveFile}
               >
                 <span className="absolute inset-0 translate-x-0 translate-y-0 bg-indigo-600 transition-transform group-hover:translate-y-0.5 group-hover:translate-x-0.5"></span>
 
-                <span className="relative block border border-current bg-white px-8 py-3">
+                <span className="relative cursor-pointer block border border-current bg-white px-8 py-3">
                   Download Resume
                 </span>
               </a>
