74563569304981546ec1f5724439bf29fa4a926d
JOB MONARI <jobmonari7@gmail.com>
2024-05-01 13:53:12 +0300

    updating the Hero with Python and Elixir 🔥

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -31,7 +31,7 @@ export const Hero = () => {
               </h1>
             </div>
             <div className="mb-4 mt-4 leading-relaxed text-justify">
-              Hello, I am a full-stack and Android developer proficient in Typescript, Kotlin and Golang. 
+              Hello, I am a full-stack and Android developer proficient in Typescript, Kotlin and Golang. I also enjoy coding in Python and ELixir 🔥.
               If you're interested in my skills and experience,please feel free to download my resume by clicking the button
               below.
             </div>
